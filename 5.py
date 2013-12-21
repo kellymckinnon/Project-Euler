82e50a8f0335fde41253e046e21a7fe8623c354e
kelly <kellymckinnon@utexas.edu>
2013-12-21 00:56:01 -0600

    5 done

--- 5.py
+++ 5.py
@@ -1,1 +1,10 @@
 __author__ = 'kelly'
+
+#finds smallest positive number that is evenly divisible by 1-20
+
+divisors = [20, 19, 18, 17, 16, 15, 14, 13, 12, 11]
+
+for x in xrange(2520, 999999999, 20):
+    if all(x % y == 0 for y in divisors):
+        print x
+print None
